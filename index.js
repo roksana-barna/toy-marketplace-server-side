94fc419d0ba53ab3f31939e537ba4aec2ba3e52c
roksana-barna <roksanabarna1803@gmail.com>
2023-05-19 01:52:28 +0600

    db a data

--- index.js
+++ index.js
@@ -22,7 +22,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const database=client.db('toysDB');
+    const toysCollection=database.collection('toys')
+    app.post('/toys',async(req,res)=>{
+        const toy=req.body;
+        console.log('new toys',toy)
+        const result =await toysCollection.insertOne(toy);
+        res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
