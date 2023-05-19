2aa2327a82b24d7f21e2721f229ef26411d865f2
roksana-barna <roksanabarna1803@gmail.com>
2023-05-20 02:56:01 +0600

    view details

--- index.js
+++ index.js
@@ -91,6 +91,20 @@ async function run() {
       const result = await toysCollection.deleteOne(query);
       res.send(result);
     })
+    // 
+    app.get('/toys',(req, res) => {
+      const limit = parseInt(req.query.limit) || 20;
+      res.send(toys.slice(0, limit));
+
+    })
+    // view details
+    app.get('/viewdetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.findOne(query);
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
