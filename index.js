3626828c53aabc132f8bbd12c6e2580e6d60cd7f
roksana-barna <roksanabarna1803@gmail.com>
2023-05-21 20:03:43 +0600

    vercel

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 // middleware
-app.use(cors());
+app.use(cors(corsOptions))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lvcap8y.mongodb.net/?retryWrites=true&w=majority`;
